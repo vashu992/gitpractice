6900086eb6c925ef0540ea3a5df61a45e49e4b96
vashu992 <ashutoshpratap992@gmail.com>
2023-03-13 14:46:49 +0530

    incomplete

---
+++ importableExecutable/calculator/calc.go
@@ -0,0 +1,17 @@
+package calculator
+
+func Add(a , b int) int{
+	return a+b
+}
+func Sub(a , b int) int{
+	return a-b
+}
+func Multyply(a , b int) int{
+	return a*b
+}
+func Divide(a , b int) int{
+	return a/b
+}
+func main() {
+	
+}
