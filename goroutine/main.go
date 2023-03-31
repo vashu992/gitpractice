2ceead0d5146b7c384bacb1753dc78ecb2e9ef24
vashu992 <ashutoshpratap992@gmail.com>
2023-03-31 14:10:46 +0530

    goroutine

---
+++ goroutine/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// // parallel
+	// -----------|------|--------------> process a or function a
+	// -----------|------|--------------> process b or funnction b
+	// -----------|------|--------------> process c or function c
+	// |1->main| 2 ->a|
+	// |3 -> b| 4 -> c|
+
+	// // Concurrency
+	// ------- ------|-- - - - ----|--- --> process d or function d
+	//        -      |   - - - -   |   -  > process e or function e
+	// |1->main or d| 2 ->a or d,e|
+	// |3 -> b| 4 -> c|
+
+	a()
+	b()
+	c()
+	d()
+
+
+	go a()
+	go b()
+	go c()
+	go d()
+	time.Sleep(time.Second * 3)
+	
+}
+
+func a() {
+	for i := 1 ; i <= 2 ; i++ {
+	fmt.Println(" Func a run hua hai, i = ",i)
+	}
+}
+func b() {
+    for i := 1 ; i <= 2 ; i++ {
+		fmt.Println(" Func b run hua hai, i = ",i)
+		}
+}
+func c() {
+	for i := 1 ; i <= 2 ; i++ {
+		fmt.Println(" Func c run hua hai, i = ",i)
+		}
+}
+func d() {
+	for i := 1 ; i <= 2 ; i++ {
+		fmt.Println(" Func d run hua hai, i = ",i)
+		}
+}
